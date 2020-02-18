425c1d155a5beffbccb9cf66b4cf685131904f63
metmirr <metin7087@gmail.com>
2020-02-18 23:08:04 +0300

    Add rate endpoint

--- exchange_api/api/views.py
+++ exchange_api/api/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from collections import namedtuple
 
-# Create your views here.
+from django.conf import settings
+from django.http import JsonResponse
+import requests
+
+
+def rate(request):
+    code = request.GET.get("code")
+    if code is None:
+        return JsonResponse(
+            {"error": "Please provide a currency code"}, status=400
+        )
+    code = code.lower()
+
+    rates = []
+    for provider in settings.PROVIDERS:
+        resp = requests.get(provider["url"])
+
+        code_lookup_key = provider["code"]
+        rate_lookup_key = provider["rate"]
+
+        for d in resp.json():
+            if d[code_lookup_key] == code:
+                rates.append(d[rate_lookup_key])
+
+    return JsonResponse(min(rates), safe=False)
